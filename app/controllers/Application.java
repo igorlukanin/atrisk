86d1a9ca5bc9affa3a8504e61283ba5d52baa47f
Igor Lukanin <mail@igor.lukanin.name>
2012-01-13 14:59:50 +0600

    Initial cleanup

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,9 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import java.util.*;
-
-import models.*;
+import play.mvc.Controller;
 
 public class Application extends Controller {
-
     public static void index() {
         render();
     }
-
 }
